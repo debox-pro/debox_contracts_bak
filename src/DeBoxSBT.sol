cbf45b6a65aaba04beb902a5563920c1de6a1209
debox <a@debox.pro>
2024-05-15 20:35:34 +0800

    sbt

---
+++ src/DeBoxSBT.sol
@@ -0,0 +1,59 @@
+//   _____    ______   ____     ____   __   __
+//  |  __ \  |  ____| |  _ \   / __ \  \ \ / /
+//  | |  | | | |__    | |_) | | |  | |  \ V /
+//  | |  | | |  __|   |  _ <  | |  | |   > <
+//  | |__| | | |____  | |_) | | |__| |  / . \
+//  |_____/  |______| |____/   \____/  /_/ \_\
+//
+//  Author: https://debox.pro/
+//
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract DeboxSBT is ERC1155Supply, Ownable {
+  using Strings for uint256;
+
+  string public constant name = "Debox Soulbound Token";
+  string public constant symbol = "DBXSBT";
+
+  string private _baseURI;
+
+  constructor(string memory uri_) ERC1155("") Ownable(msg.sender) {
+    _baseURI = uri_;
+  }
+
+  function uri(uint256 id) public view override returns (string memory) {
+    require(exists(id), "DeboxSBT: token not exist");
+    return bytes(_baseURI).length > 0 ? string.concat(_baseURI, id.toString()) : "";
+  }
+
+  function mint(uint256 tokenId, address[] calldata recipients) external onlyOwner {
+    for (uint256 i = 0; i < recipients.length; i++) {
+      _mint(recipients[i], tokenId, 1, "");
+    }
+  }
+
+  function burn(uint256 tokenId, uint256 value) external {
+    _burn(msg.sender, tokenId, value);
+  }
+
+  function setURI(string calldata baseURI) external onlyOwner {
+    _baseURI = baseURI; //ignore event
+  }
+
+  function setApprovalForAll(address, bool) public pure override(ERC1155) {
+    revert("disabled");
+  }
+
+  function _update(address from, address to, uint256[] memory ids, uint256[] memory values) internal override {
+    // only allow minting or burning, not transfers
+    require(from == address(0) || (from != address(0) && to == address(0)), "transfers not allowed");
+    super._update(from, to, ids, values);
+  }
+}
---
+++ test/SBT.t.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { Test, console } from "forge-std/Test.sol";
+import { DeboxSBT } from "../src/DeboxSBT.sol";
+
+contract SBTTest is Test {
+  DeboxSBT sbt;
+
+  address admin = makeAddr("admin");
+
+  function setUp() public {
+    vm.prank(admin);
+    sbt = new DeboxSBT("https://data.debox.pro/nft/sbt/");
+  }
+
+  function testAirdrop(uint256 count) public {
+    vm.assume(count > 0 && count < 100);
+
+    address[] memory users = new address[](count);
+    for (uint256 i = 0; i < users.length; i++) {
+      users[i] = makeAddr(string.concat("user", vm.toString(i)));
+    }
+    vm.prank(admin);
+    sbt.mint(1, users);
+
+    assertEq(sbt.totalSupply(1), count, "total supply should be equal to count");
+
+    for (uint256 i = 0; i < users.length; i++) {
+      assertEq(sbt.balanceOf(users[i], 1), 1, string.concat("abc:", vm.toString(users[i])));
+    }
+  }
+
+  function testSBTURL() public {
+    address bob = makeAddr("bob");
+    airdrop(bob);
+    assertEq(sbt.uri(1), "https://data.debox.pro/nft/sbt/1");
+
+    vm.prank(admin);
+    sbt.setURI("https://data.debox.pro/");
+    assertEq(sbt.uri(1), "https://data.debox.pro/1");
+  }
+
+  function testFailedAirdropRole() public {
+    address[] memory users = new address[](1);
+    users[0] = makeAddr("bob");
+    sbt.mint(1, users);
+  }
+
+  function testTransfer() public {
+    address bob = makeAddr("bob");
+    airdrop(bob);
+
+    vm.expectRevert("disabled");
+    vm.prank(bob);
+    sbt.setApprovalForAll(address(this), true);
+
+    vm.expectRevert("transfers not allowed");
+    vm.prank(bob);
+    sbt.safeTransferFrom(bob, makeAddr("alice"), 1, 1, "");
+
+    // allow burn
+    vm.prank(bob);
+    sbt.burn(1, 1);
+  }
+
+  function airdrop(address to) public {
+    address[] memory users = new address[](1);
+    users[0] = to;
+
+    vm.prank(admin);
+    sbt.mint(1, users);
+  }
+}
