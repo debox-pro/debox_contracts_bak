5479f02825ee8400dc3205f0541197d6cceae2ce
coder <coder@debox.pro>
2024-08-08 10:38:29 +0800

    reset evm version

--- src/BoxLockup.sol
+++ src/BoxLockup.sol
@@ -9,7 +9,7 @@
 //
 
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
